1efbbaf12c7288f2517d8b09e488d12a6887fe7a
hsiehm <mario.hsieh@autodesk.com>
2017-06-26 16:41:57 -0700

    added comments explaining the logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-    $(document).on('click', '.page-link', function() {
-        var page = $(this).attr('href');
+
+    // event handler for nav menu click
+    $('.page-link').on('click', function() {
+        var page = $(this).attr('href'); // get the page ID from href attribute
 
         $('html, body').animate({
-            scrollTop: $(page).offset().top
+            scrollTop: $(page).offset().top // logic to dynamically scroll to desired page
         }, 1000);
     });
 });
